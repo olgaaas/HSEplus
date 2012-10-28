9ae51b51e57728090fc40611b8da29e28c5e9425
Alexey Khan <axenwork@gmail.com>
2012-10-28 16:35:13 +0400

    Added New Entrant Class

---
+++ Entrant.h
@@ -0,0 +1,13 @@
+//
+//  Entrant.h
+//  HSE+
+//
+//  Created by  Alexey Khan on 28.10.12.
+//  Copyright (c) 2012 Alexey Khan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Entrant : NSObject
+
+@end
---
+++ Entrant.m
@@ -0,0 +1,13 @@
+//
+//  Entrant.m
+//  HSE+
+//
+//  Created by  Alexey Khan on 28.10.12.
+//  Copyright (c) 2012 Alexey Khan. All rights reserved.
+//
+
+#import "Entrant.h"
+
+@implementation Entrant
+
+@end
