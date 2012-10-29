ca099171c1f25e0f9c0b49021cbc992872ca4fe0
Olga <anoniav@gmail.com>
2012-10-30 02:20:47 +0400

    добавлен класс Reminder

---
+++ Reminder.h
@@ -0,0 +1,37 @@
+//
+//  Reminder.h
+//  
+//
+//  Created by Olga Kolekonova on 10/29/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Reminder : NSObject{
+    NSString *DateRemind;//
+    NSString *TimeRemind;//дата ремайндера;
+    NSString *DateEvent;//время события;
+    NSString *TimeEvent;//дата события;
+    NSString *Contain;//содержание ремайндера;
+    NSString *newsWms;// переменная для новость/сообщение от старосты;
+    BOOL *SendWarden; //если сообщение от старосты, отправлять на почту.
+}
+
+-(NSString *)DateRemind;
+-(NSString *)TimeRemind;
+-(NSString *)DateEvent;
+-(NSString *)TimeEvent;
+-(NSString *)Contain;
+-(NSString *)newsWms;
+-(BOOL *)SendWarden;
+
+-(void)setDateRemind:(NSString *)_DateRemind;
+-(void)setTimeRemind:(NSString *)_TimeRemind;
+-(void)setDateEvent:(NSString *)_DateEvent;
+-(void)setTimeEvent:(NSString *)_TimeEvent;
+-(void)setContain:(NSString *)_Contain;
+-(void)setWms:(NSString *)_newsWms;
+-(void)setSendWarden(BOOL *)_SendWarden;
+    
+@end
---
+++ Reminder.m
@@ -0,0 +1,32 @@
+//
+//  Reminder.m
+//  
+//
+//  Created by Olga Kolekonova on 10/29/12.
+//
+//
+
+#import "Reminder.h"
+
+@implementation Reminder
+
+
+
+-(NSString *)DateRemind {return DateRemind;}
+-(NSString *)TimeRemind{return TimeRemind;}
+-(NSString *)DateEvent{return DateEvent;}
+-(NSString *)TimeEvent{return TimeEvent;}
+-(NSString *)Contain{return Contain;}
+-(NSString *)newsWms{return newsWms;}
+-(BOOL *)SendWarden{return SendWarden;}
+
+-(void)setDateRemind:(NSString *)_DateRemind{DateRemind = _DateRemind};
+-(void)setTimeRemind:(NSString *)_TimeRemind {TimeRemind = _TimeRemind};
+-(void)setDateEvent:(NSString *)_DateEvent{DateEvent = _DateEvent};
+-(void)setTimeEvent:(NSString *)_TimeEvent{TimeEvent = _TimeEvent};
+-(void)setContain:(NSString *)_Contain {Contain = _Contain};
+-(void)setWms:(NSString *)_newsWms {newsWms = _newsWms};
+-(void)setSendWarden(BOOL *)_SendWarden {SendWarden = _SendWarden};
+
+
+@end
